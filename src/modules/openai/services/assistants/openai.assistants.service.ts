e917f205d272649f6397d9820a148a26c01d8b5c
LordCail1 <rami.batnigi@gmail.com>
2023-12-14 14:42:39 -0500

    Refactor OpenaiAssistantsService methods

--- src/modules/openai/services/assistants/openai.assistants.service.ts
+++ src/modules/openai/services/assistants/openai.assistants.service.ts
@@ -15,39 +15,35 @@ import { OpenaiAbstractService } from '../openai.abstract.service';
 export class OpenaiAssistantsService extends OpenaiAbstractService {
   /**
    * This method is responsible for listing all the assistants
-   * @returns - an array of assistants
+   * @returns An array of assistants
    */
   async listAllAssistants(): Promise<AssistantsPage> {
     try {
-      return this.openai.beta.assistants.list();
+      return await this.openai.beta.assistants.list();
     } catch (error) {
       throw new HttpException(
-        {
-          message: 'Something went wrong listing all the assistants',
-          error,
-        },
+        'Something went wrong listing all the assistants',
         HttpStatus.INTERNAL_SERVER_ERROR,
+        { cause: error },
       );
     }
   }
 
   /**
    * This method is responsible for creating a new assistant
-   * @param assistantCreateParams - the assistant create params
-   * @returns - the assistant object
+   * @param assistantCreateParams The assistant create params
+   * @returns The assistant object
    */
   async createAssistant(
     assistantCreateParams: AssistantCreateParams,
   ): Promise<Assistant> {
     try {
-      return this.openai.beta.assistants.create(assistantCreateParams);
+      return await this.openai.beta.assistants.create(assistantCreateParams);
     } catch (error) {
       throw new HttpException(
-        {
-          message: `Something went wrong creating the assistant ${assistantCreateParams.name}`,
-          error,
-        },
+        `Something went wrong creating the assistant ${assistantCreateParams.name}`,
         HttpStatus.INTERNAL_SERVER_ERROR,
+        { cause: error },
       );
     }
   }
