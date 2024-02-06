6f9fb16c7982e3996d558683acbac15d501a958a
LordCail1 <rami.batnigi@gmail.com>
2024-02-06 06:37:27 -0500

    HAV-10 added bearer token authentication guard

---
+++ src/guards/bearerToken.guard.spec.ts
@@ -0,0 +1,8 @@
+import { ConfigService } from '@nestjs/config';
+import { BearerTokenGuard } from './bearerToken.guard';
+
+describe('GuardsGuard', () => {
+  it('should be defined', () => {
+    expect(new BearerTokenGuard(new ConfigService())).toBeDefined();
+  });
+});
