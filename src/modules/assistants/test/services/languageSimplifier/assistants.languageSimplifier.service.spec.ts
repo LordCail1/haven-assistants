948d4b261c8bf9bad31815c59ba987f153791679
LordCail1 <rami.batnigi@gmail.com>
2024-05-13 15:09:31 -0400

    started working on unit test for the new `languageSimplifier` assistant

---
+++ src/modules/assistants/test/services/languageSimplifier/assistants.languageSimplifier.service.spec.ts
@@ -0,0 +1,9 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AssistantsLanguageParserService', () => {
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [],
+    }).compile();
+  });
+});
